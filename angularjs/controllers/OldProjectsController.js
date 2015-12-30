0417d8d0879f7d8c36759905e7502d52d5863d97
Brandon Groff <mynamesbg@gmail.com>
2015-12-30 02:59:18 -0500

    Added some css effects, add nav to imagePopup.
    TODO: Fix main nav bar and header spacing with the shine effect.

--- angularjs/controllers/OldProjectsController.js
+++ angularjs/controllers/OldProjectsController.js
@@ -2,11 +2,29 @@ app.controller("OldProjectsController", function ($scope, $location, $json) {
 
     $scope.showPopup = false;
 	$scope.popupImage = null;
+	var currentProj = null;
+	var currentImg = null;
 	
-	$scope.showImagePopup = function(image){
+	$scope.showImagePopup = function(image, imgIndex, parentIndex){
 		$scope.showPopup = true;
 		$scope.popupImage = image;
-		console.log("Show popup");
+		currentProj = parentIndex;
+		currentImg = imgIndex;
+	};
+	
+	$scope.popupNavigation = function (nav) {
+		if (currentImg != null) {
+			var next = currentImg + nav;
+			if (next < 0){ // below valid range, go to last
+				next = $scope.projects[currentProj].images.length-1;
+			} else if (next >= $scope.projects[currentProj].images.length){ // exceeds valid range
+				next = 0;
+			} else {
+				//next is valid
+			}
+			currentImg = next;
+			$scope.popupImage = $scope.projects[currentProj].images[currentImg];
+		};
 	};
 	
 	$scope.singleProjectClick = function (title) {
